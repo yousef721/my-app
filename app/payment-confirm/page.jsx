97caa76e4b022d7f86d2a97ce575c874b2248709
Yousef Abdullah <yousarsefabedalla255@gmail.com>
2024-03-20 23:09:55 +0200

    first commit

--- app/payment-confirm/page.jsx
+++ app/payment-confirm/page.jsx
@@ -5,7 +5,7 @@ import React from "react";
 function PaymentConfirm() {
   return (
     <div className="flex flex-col items-center justify-center px-5 mt-4">
-      <Image src="/public/verified.gif" alt="check" width={130} height={130} />
+      <Image src="/verified.gif" alt="check" width={130} height={130} />
       <h2 className="text-[24px]">Payment Successful !</h2>
       <h2 className="text-[17px] text-center mt-6 text-gray-500">
         We sent an email with your order confirmation along with Digital Content
